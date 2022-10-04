a3aeb0d9f7e80accd231d7c519ac512ed8197a1a
Harper-BH <114521184+Harper-BH@users.noreply.github.com>
2022-10-04 16:34:12 -0400

    Add files via upload

---
+++ Module 4/4.1.py
@@ -0,0 +1,20 @@
+
+def max3(a, b, c):
+    num1 = float(a)
+    num2 = float(b)
+    num3 = float(c)
+    return max(num1, num2, num3)
+
+
+def do3():
+    num1 = input("Number 1:")
+    num2 = input("Number 2:")
+    num3 = input("Number 3:")
+
+    print (max3(num1, num2, num3))
+
+
+
+do3()
+do3()
+do3()
