cdb9a8b7d0cdeecd5b97073ebfc4cd30d86eceed
Harper-BH <114521184+Harper-BH@users.noreply.github.com>
2022-10-25 16:57:11 -0400

    Add files via upload

---
+++ Module 5/5.1.py
@@ -0,0 +1,26 @@
+largest = None
+smallest = None
+avg = None
+range = None
+count = 0
+sum = 0
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+        n = int(num)
+        if largest is None or n > largest :
+            largest = n
+        elif smallest is None or smallest > n :
+            smallest = n
+        sum = sum + n
+        count = count + 1
+    except:
+        print("Invalid input")
+avg = sum / count
+range = int(largest) - int(smallest)
+
+print("Average is", avg)
+print("Range is", range)
+print("Count is", count)
